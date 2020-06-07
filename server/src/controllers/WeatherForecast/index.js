357330bdbae8ba30fb4b24c2b2663b204154a1e6
yordan511 <yordangalabov511@gmail.com>
2020-06-07 20:53:08 +0300

    implement get weather api endpoint

--- server/src/controllers/WeatherForecast/index.js
+++ server/src/controllers/WeatherForecast/index.js
@@ -1,13 +1,17 @@
 const Router = require("koa-router");
 const passport = require("passport");
 const koaBody = require("koa-body");
+const fetch = require("node-fetch");
 
 const router = new Router({
   prefix: "/weather",
 });
 
+const route = `http://api.openweathermap.org/data/2.5/forecast/daily?q=Sofia&units=metric&cnt=7&appid=${process.env.OPEN_WEATHER_KEY}`;
+
 router.get("/", async (ctx) => {
-  ctx.body = "this route is authenticated";
+  const openWeatherData = await fetch(route);
+  ctx.body = await openWeatherData.json();
 });
 
 module.exports = router;
